676b9f6aa42ba6b0d79923574c2c0695b2ee9f24
William Lim <william.lim2@telus.com>
2022-10-02 16:11:47 -0700

    Refactor app

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import {
   StyleSheet,
-  Text,
   View,
   TextInput,
-  Pressable,
   Keyboard,
   KeyboardAvoidingView,
 } from 'react-native'
 import { useState } from 'react'
-import { palette } from './theme/palette'
 import Header from './src/components/Header'
 import RemoveButton from './src/components/RemoveButton'
 import DismissKeyboard from './src/components/DismissKeyboard'
+import ListItem from './src/components/ListItem'
 
 const App = () => {
   const [input, setInput] = useState('')
@@ -38,24 +36,6 @@ const App = () => {
     }
   }
 
-  const Item = ({ isChecked, value, index }) => (
-    <View style={{ marginTop: 10 }}>
-      <View style={styles.row}>
-        <View style={styles.col1}>
-          <Pressable
-            style={isChecked ? styles.checked : styles.unChecked}
-            onPress={() => handleClickCheckBox(index)}
-          />
-        </View>
-        <View style={styles.col11}>
-          <Text style={{ fontSize: 17, justifyContent: 'center', height: 22 }}>
-            {value}
-          </Text>
-        </View>
-      </View>
-    </View>
-  )
-
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
@@ -77,11 +57,12 @@ const App = () => {
               style={{ borderTopWidth: 1, marginTop: 15, marginBottom: 5 }}
             />
             {list.map(({ text, isChecked }, index) => (
-              <Item
+              <ListItem
                 key={index}
                 value={text}
                 isChecked={isChecked}
                 index={index}
+                onPress={handleClickCheckBox}
               />
             ))}
           </View>
@@ -115,40 +96,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'black',
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'flex-end',
-  },
-  col1: { flex: 1 },
-  col2: { flex: 2 },
-  col3: { flex: 3 },
-  col4: { flex: 4 },
-  col5: { flex: 5 },
-  col6: { flex: 6 },
-  col7: { flex: 7 },
-  col8: { flex: 8 },
-  col9: { flex: 9 },
-  col10: { flex: 10 },
-  col11: { flex: 11 },
-  checked: {
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: '1px',
-    borderRadius: '50%',
-    height: 25,
-    width: 25,
-    backgroundColor: palette.primary.main,
-  },
-  unChecked: {
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: '1px',
-    borderRadius: '50%',
-    height: 25,
-    width: 25,
   },
 })
 
---
+++ src/components/ListItem.js
@@ -0,0 +1,61 @@
+import {StyleSheet, Text, View, Pressable } from 'react-native'
+import { palette } from '../../theme/palette'
+
+const ListItem = ({ isChecked, value, index, onPress }) => {
+  return (
+    <View style={{ marginTop: 10 }}>
+      <View style={styles.row}>
+        <View style={styles.col1}>
+          <Pressable
+            style={isChecked ? styles.checked : styles.unChecked}
+            onPress={() => onPress(index)}
+          />
+        </View>
+        <View style={styles.col11}>
+          <Text style={{ fontSize: 17, justifyContent: 'center', height: 22 }}>
+            {value}
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  checked: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: '1px',
+    borderRadius: '50%',
+    height: 25,
+    width: 25,
+    backgroundColor: palette.primary.main,
+  },
+  unChecked: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: '1px',
+    borderRadius: '50%',
+    height: 25,
+    width: 25,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-end',
+  },
+  col1: { flex: 1 },
+  col2: { flex: 2 },
+  col3: { flex: 3 },
+  col4: { flex: 4 },
+  col5: { flex: 5 },
+  col6: { flex: 6 },
+  col7: { flex: 7 },
+  col8: { flex: 8 },
+  col9: { flex: 9 },
+  col10: { flex: 10 },
+  col11: { flex: 11 },
+})
+
+export default ListItem
